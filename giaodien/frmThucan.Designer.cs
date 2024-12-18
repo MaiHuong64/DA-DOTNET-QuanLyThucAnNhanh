eea6e9687e0303a755c226e5bde74fa0ef02d84e
MaiHuong64 <rain06042004@gmail.com>
2024-12-19 04:17:17 +0700

    them event form thuc an

--- giaodien/frmThucan.Designer.cs
+++ giaodien/frmThucan.Designer.cs
@@ -33,37 +33,38 @@
             this.btnReload = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.cmbLoai = new System.Windows.Forms.ComboBox();
+            this.cbbLoai = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.nbSoluong = new System.Windows.Forms.NumericUpDown();
-            this.cmbSanpham = new System.Windows.Forms.ComboBox();
+            this.numSL = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtDongia = new System.Windows.Forms.TextBox();
+            this.txtDG = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.txtMaDA = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.grp3 = new System.Windows.Forms.GroupBox();
             this.btnTimkiem = new System.Windows.Forms.Button();
             this.txtTimkiem = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.dtMon = new System.Windows.Forms.DataGridView();
+            this.dataFood = new System.Windows.Forms.DataGridView();
+            this.txtTenDA = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nbSoluong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSL)).BeginInit();
             this.grp3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtMon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataFood)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSua
             // 
             this.btnSua.BackColor = System.Drawing.Color.Sienna;
             this.btnSua.ForeColor = System.Drawing.Color.White;
-            this.btnSua.Location = new System.Drawing.Point(732, 230);
+            this.btnSua.Location = new System.Drawing.Point(732, 217);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(93, 42);
             this.btnSua.TabIndex = 22;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnReload
             // 
@@ -75,50 +76,52 @@
             this.btnReload.TabIndex = 21;
             this.btnReload.Text = "Reload";
             this.btnReload.UseVisualStyleBackColor = false;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
             // 
             // btnThem
             // 
             this.btnThem.BackColor = System.Drawing.Color.Sienna;
             this.btnThem.ForeColor = System.Drawing.Color.White;
-            this.btnThem.Location = new System.Drawing.Point(732, 154);
+            this.btnThem.Location = new System.Drawing.Point(732, 141);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(93, 42);
             this.btnThem.TabIndex = 20;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.cmbLoai);
+            this.groupBox1.Controls.Add(this.txtTenDA);
+            this.groupBox1.Controls.Add(this.cbbLoai);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.nbSoluong);
-            this.groupBox1.Controls.Add(this.cmbSanpham);
+            this.groupBox1.Controls.Add(this.numSL);
             this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.txtDongia);
+            this.groupBox1.Controls.Add(this.txtDG);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.txtMaSP);
+            this.groupBox1.Controls.Add(this.txtMaDA);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(63, 101);
+            this.groupBox1.Location = new System.Drawing.Point(63, 88);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(639, 226);
             this.groupBox1.TabIndex = 19;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin";
             // 
-            // cmbLoai
+            // cbbLoai
             // 
-            this.cmbLoai.FormattingEnabled = true;
-            this.cmbLoai.Location = new System.Drawing.Point(138, 170);
-            this.cmbLoai.Name = "cmbLoai";
-            this.cmbLoai.Size = new System.Drawing.Size(154, 30);
-            this.cmbLoai.TabIndex = 49;
+            this.cbbLoai.FormattingEnabled = true;
+            this.cbbLoai.Location = new System.Drawing.Point(138, 129);
+            this.cbbLoai.Name = "cbbLoai";
+            this.cbbLoai.Size = new System.Drawing.Size(154, 30);
+            this.cbbLoai.TabIndex = 49;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(18, 178);
+            this.label2.Location = new System.Drawing.Point(18, 137);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 22);
             this.label2.TabIndex = 48;
@@ -134,54 +137,46 @@
             this.label8.TabIndex = 22;
             this.label8.Text = "Số lượng tồn";
             // 
-            // nbSoluong
+            // numSL
             // 
-            this.nbSoluong.Location = new System.Drawing.Point(460, 28);
-            this.nbSoluong.Name = "nbSoluong";
-            this.nbSoluong.Size = new System.Drawing.Size(158, 30);
-            this.nbSoluong.TabIndex = 20;
-            // 
-            // cmbSanpham
-            // 
-            this.cmbSanpham.FormattingEnabled = true;
-            this.cmbSanpham.Location = new System.Drawing.Point(138, 99);
-            this.cmbSanpham.Name = "cmbSanpham";
-            this.cmbSanpham.Size = new System.Drawing.Size(154, 30);
-            this.cmbSanpham.TabIndex = 18;
+            this.numSL.Location = new System.Drawing.Point(460, 28);
+            this.numSL.Name = "numSL";
+            this.numSL.Size = new System.Drawing.Size(158, 30);
+            this.numSL.TabIndex = 20;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label6.Location = new System.Drawing.Point(18, 107);
+            this.label6.Location = new System.Drawing.Point(18, 89);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 22);
             this.label6.TabIndex = 17;
             this.label6.Text = "Sản phẩm";
             // 
-            // txtDongia
+            // txtDG
             // 
-            this.txtDongia.Location = new System.Drawing.Point(460, 94);
-            this.txtDongia.Name = "txtDongia";
-            this.txtDongia.Size = new System.Drawing.Size(158, 30);
-            this.txtDongia.TabIndex = 13;
+            this.txtDG.Location = new System.Drawing.Point(460, 76);
+            this.txtDG.Name = "txtDG";
+            this.txtDG.Size = new System.Drawing.Size(158, 30);
+            this.txtDG.TabIndex = 13;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label4.Location = new System.Drawing.Point(359, 102);
+            this.label4.Location = new System.Drawing.Point(359, 84);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 22);
             this.label4.TabIndex = 12;
             this.label4.Text = "Đơn giá";
             // 
-            // txtMaSP
+            // txtMaDA
             // 
-            this.txtMaSP.Location = new System.Drawing.Point(138, 32);
-            this.txtMaSP.Name = "txtMaSP";
-            this.txtMaSP.Size = new System.Drawing.Size(154, 30);
-            this.txtMaSP.TabIndex = 9;
+            this.txtMaDA.Location = new System.Drawing.Point(138, 32);
+            this.txtMaDA.Name = "txtMaDA";
+            this.txtMaDA.Size = new System.Drawing.Size(154, 30);
+            this.txtMaDA.TabIndex = 9;
             // 
             // label1
             // 
@@ -198,7 +193,7 @@
             this.grp3.Controls.Add(this.btnTimkiem);
             this.grp3.Controls.Add(this.btnReload);
             this.grp3.Controls.Add(this.txtTimkiem);
-            this.grp3.Location = new System.Drawing.Point(335, 326);
+            this.grp3.Location = new System.Drawing.Point(335, 313);
             this.grp3.Name = "grp3";
             this.grp3.Size = new System.Drawing.Size(490, 67);
             this.grp3.TabIndex = 47;
@@ -214,6 +209,7 @@
             this.btnTimkiem.TabIndex = 48;
             this.btnTimkiem.Text = "Tìm kiếm";
             this.btnTimkiem.UseVisualStyleBackColor = false;
+            this.btnTimkiem.Click += new System.EventHandler(this.btnTimkiem_Click);
             // 
             // txtTimkiem
             // 
@@ -222,6 +218,7 @@
             this.txtTimkiem.Name = "txtTimkiem";
             this.txtTimkiem.Size = new System.Drawing.Size(218, 30);
             this.txtTimkiem.TabIndex = 25;
+            this.txtTimkiem.Leave += new System.EventHandler(this.txtTimkiem_Leave);
             // 
             // label3
             // 
@@ -230,20 +227,28 @@
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.label3.Location = new System.Drawing.Point(54, 19);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(222, 49);
+            this.label3.Size = new System.Drawing.Size(145, 49);
             this.label3.TabIndex = 18;
-            this.label3.Text = "THỨC ĂN";
+            this.label3.Text = "FOOD";
             // 
-            // dtMon
+            // dataFood
             // 
-            this.dtMon.BackgroundColor = System.Drawing.Color.PapayaWhip;
-            this.dtMon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtMon.Location = new System.Drawing.Point(63, 399);
-            this.dtMon.Name = "dtMon";
-            this.dtMon.RowHeadersWidth = 51;
-            this.dtMon.RowTemplate.Height = 24;
-            this.dtMon.Size = new System.Drawing.Size(762, 130);
-            this.dtMon.TabIndex = 17;
+            this.dataFood.BackgroundColor = System.Drawing.Color.PapayaWhip;
+            this.dataFood.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataFood.Location = new System.Drawing.Point(63, 400);
+            this.dataFood.Name = "dataFood";
+            this.dataFood.RowHeadersWidth = 51;
+            this.dataFood.RowTemplate.Height = 24;
+            this.dataFood.Size = new System.Drawing.Size(762, 130);
+            this.dataFood.TabIndex = 17;
+            this.dataFood.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataFood_CellClick);
+            // 
+            // txtTenDA
+            // 
+            this.txtTenDA.Location = new System.Drawing.Point(138, 76);
+            this.txtTenDA.Name = "txtTenDA";
+            this.txtTenDA.Size = new System.Drawing.Size(154, 30);
+            this.txtTenDA.TabIndex = 50;
             // 
             // frmThucan
             // 
@@ -256,18 +261,19 @@
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.dtMon);
+            this.Controls.Add(this.dataFood);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmThucan";
             this.Text = "frmThucan";
+            this.Load += new System.EventHandler(this.frmThucan_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nbSoluong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSL)).EndInit();
             this.grp3.ResumeLayout(false);
             this.grp3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtMon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataFood)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -280,19 +286,19 @@
         private System.Windows.Forms.Button btnThem;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.NumericUpDown nbSoluong;
-        private System.Windows.Forms.ComboBox cmbSanpham;
+        private System.Windows.Forms.NumericUpDown numSL;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtDongia;
+        private System.Windows.Forms.TextBox txtDG;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.TextBox txtMaDA;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DataGridView dtMon;
+        private System.Windows.Forms.DataGridView dataFood;
         private System.Windows.Forms.GroupBox grp3;
         private System.Windows.Forms.TextBox txtTimkiem;
-        private System.Windows.Forms.ComboBox cmbLoai;
+        private System.Windows.Forms.ComboBox cbbLoai;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnTimkiem;
+        private System.Windows.Forms.TextBox txtTenDA;
     }
 }
--- giaodien/frmThucan.cs
+++ giaodien/frmThucan.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +13,229 @@ namespace giaodien
         {
             InitializeComponent();
         }
-        
+        public void LoadData()
+        {
+            try
+            {
+                using (SqlConnection conn = Ketnoi.getConnection())
+                {
+                    string query = @"select d.mada, d.tenda, l.tenloai, d.dongia, d.soluong, l.maloai
+                             from doan d, loaidoan l
+                             where d.maloai = l.maloai";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataFood.DataSource = dt;
+                    LoadTable();
+                    //dataFood.Columns["mada"].Visible = false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void LoadCBBSP()
+        {
+            try
+            {
+                using (SqlConnection con = Ketnoi.getConnection())
+                {
+                    string query = @"select *
+                                    from loaidoan";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    cbbLoai.DataSource = dt;
+                    cbbLoai.DisplayMember = "tenloai";
+                    cbbLoai.ValueMember = "maloai";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void LoadTable()
+        {
+            dataFood.Columns["mada"].HeaderText = "Ma do an";
+            dataFood.Columns["tenda"].HeaderText = "Ten do an";
+            dataFood.Columns["tenloai"].HeaderText = "Loai do an";
+            dataFood.Columns["dongia"].HeaderText = "Don gia";
+            dataFood.Columns["mada"].HeaderText = "Ma do an";
+            dataFood.Columns["soluong"].HeaderText = "So luong ton";
+            dataFood.Columns["maloai"].HeaderText = "Ma loai";
+        }
+        private void frmThucan_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            LoadCBBSP();
+        }
+        public bool CheckInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaDA.Text))
+            {
+                MessageBox.Show("Vui lonh nhapp ma");
+                txtMaDA.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtTenDA.Text))
+            {
+                MessageBox.Show("Vui lonh nhapp ma ten");
+                txtTenDA.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtDG.Text))
+            {
+                MessageBox.Show("Vui lonh dongia");
+                txtDG.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool checkPrice(decimal price)
+        {
+            if (price <= 0)
+            {
+                MessageBox.Show("Vui long nhap don gia > 0");
+                return false;
+            }
+            return true;
+        }
+        public bool checkIsNum(string num)
+        {
+            if (Regex.IsMatch(num, @"^\d+$"))
+            {
+                if (checkPrice(decimal.Parse(num)))
+                    return true;
+            }
+            return false;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!CheckInput()) return;
+            if (!checkIsNum(txtDG.Text)) return;
+
+            //Kiem tra do an ton tai
+            try
+            {
+                string checkquery = @"select count(*) from doan where tenda = @tenda";
+                using (SqlConnection con = Ketnoi.getConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(checkquery, con);
+                    cmd.Parameters.AddWithValue("tenda", txtTenDA.Text);
+                    con.Open();
+                    int res = (int)cmd.ExecuteScalar();
+                    if (res > 0)
+                    {
+                        string update = @"update doan 
+                                        set soluong = soluong + @soluong
+                                        where tenda = @tenda";
+                        SqlCommand command = new SqlCommand(update, con);
+                        command.Parameters.AddWithValue("@tenda", txtTenDA.Text);
+                        command.Parameters.AddWithValue("@soluong", (int)numSL.Value);
+                        //con.Open();
+                        command.ExecuteNonQuery();
+                        LoadData();
+                        MessageBox.Show("So luong da duoc cap nhat ");
+                    }
+                    else
+                    {
+                        string query = @"insert into doan values (@mada, @tenda, @dongia, @soluong, @maloai)";
+                        SqlCommand command = new SqlCommand(query, con);
+                        command.Parameters.AddWithValue("@mada", txtMaDA.Text);
+                        command.Parameters.AddWithValue("@tenda", txtTenDA.Text);
+                        command.Parameters.AddWithValue("@dongia", decimal.Parse(txtDG.Text));
+                        command.Parameters.AddWithValue("@soluong", (int)numSL.Value);
+                        command.Parameters.AddWithValue("@maloai", cbbLoai.SelectedValue);
+                        //con.Open();
+                        command.ExecuteNonQuery();
+                        LoadData();
+                        MessageBox.Show("Them mon thanh cong");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"update doan 
+                                    set tenda = @tenda, dongia = @dongia, soluong = @soluong
+                                    where mada = @mada";
+                using (SqlConnection conn = Ketnoi.getConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@mada", txtMaDA.Text);
+                    cmd.Parameters.AddWithValue("@tenda", txtTenDA.Text);
+                    cmd.Parameters.AddWithValue("@dongia", txtDG.Text);
+                    cmd.Parameters.AddWithValue("@soluong", (int)numSL.Value);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    LoadData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnTimkiem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"select d.mada, d.tenda, l.tenloai, d.dongia, d.soluong, l.maloai
+                             from doan d, loaidoan l
+                             where d.maloai = l.maloai and d.tenda like @tenda";
+                using (SqlConnection conn = Ketnoi.getConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@tenda", "%" + txtTimkiem.Text + "%");
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataFood.DataSource = dataTable;
+                    LoadTable();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void dataFood_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataFood.Rows[e.RowIndex];
+            if (row != null)
+            {
+                txtMaDA.Text = row.Cells["mada"].Value.ToString();
+                txtTenDA.Text = row.Cells["tenda"].Value.ToString();
+                cbbLoai.SelectedItem = row.Cells["tenloai"].Value.ToString();
+                txtDG.Text = row.Cells["dongia"].Value.ToString();
+                numSL.Value = Int32.Parse(row.Cells["soluong"].Value.ToString());
+            }
+        }
+
+        private void txtTimkiem_Leave(object sender, EventArgs e)
+        {
+            if (!txtTimkiem.Focused && txtTimkiem.Text.Trim() == string.Empty)
+                txtTimkiem.Text = "Nhập tên món";
+        }
     }
 }
